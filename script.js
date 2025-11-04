5b40c238ad213f45bf4b4b6c95320896aef84528
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-04 19:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 
 document.addEventListener('DOMContentLoaded', () => {
-	const p = document.createElement('p');
-	p.textContent = 'DOM load success';
-
-	document.body.appendChild(p);
+	setTimeout(() => {
+    const p = document.createElement('p');
+    p.textContent = 'DOM load success';
+    document.body.appendChild(p);
+  }, 100);
 	
 })
